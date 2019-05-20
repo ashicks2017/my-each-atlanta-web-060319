5ea59df2ad294776ecc084b97c21d153b5f1be12
aeisha simmons <ashicks2017@outlook.com>
2019-05-20 00:07:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,15 +3,7 @@ def my_each(array)
    yield(array[i])
     i = i + 1
 end
+   array
+end
 
-end
-def hello_t(array)
-  i = 0
- 
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
- 
-  array
-end
+my_each
